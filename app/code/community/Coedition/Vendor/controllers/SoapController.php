197c5a1c9f620c842e5b545e4318ac8e87682302
RJ Garcia <rj@bighead.net>
2018-03-04 22:33:11 -0800

    Forcing WSI Non-Compliance
    Signed-off-by: RJ Garcia <rj@bighead.net>

--- app/code/community/Coedition/Vendor/controllers/SoapController.php
+++ app/code/community/Coedition/Vendor/controllers/SoapController.php
@@ -1,28 +1,4 @@
 <?php
-/**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magento.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magento.com for more information.
- *
- * @category    Mage
- * @package     Mage_Api
- * @copyright  Copyright (c) 2006-2017 X.commerce, Inc. and affiliates (http://www.magento.com)
- * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
 
 /**
  * Soap Controller to always serve non-wsi wsdl
@@ -35,6 +11,9 @@ class Coedition_Vendor_SoapController extends Mage_Api_Controller_Action
 {
     public function indexAction()
     {
+        // force non-wsi compliance
+        Mage::app()->getStore()->setConfig(Mage_Api_Helper_Data::XML_PATH_API_WSI, false);
+
         $handler_name = 'soap_v2';
 
         /* @var $server Mage_Api_Model_Server */
